3bf0ad694a9e5356713f33f2b87b6d93d43b9bf9
Ajay Kumar <41167768+ajay300898@users.noreply.github.com>
2020-09-01 12:41:20 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,25 @@
 #include "stats.h"
+#include<cmath>
+#define NAN
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Statistics::Stats() {}
+Statistics::Stats(float avg, float min, float max) {
+    this->average = avg;
+    this->max = max;
+    this->min = min;
+}
+
+Stats Statistics::ComputeStatistics(const std::vector<___>& sample) {
+    
+    if (sample.size() == 0) {
+        return Stats(NAN,NAN,NAN);
+    }
+    float min = *min_element(sample.begin(), sample.end());
+    float max = *max_element(sample.begin(), sample.end());
+    float sum = 0;
+    for (int i = 0; i < sample.size(); i++) {
+        sum += sample[i];
+    }
+    float avg = sum / sample.size();
+    return Stats(avg, min, max);
 }
